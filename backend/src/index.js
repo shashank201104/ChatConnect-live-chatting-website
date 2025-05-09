8986748752e24fba7d08f838defd515e0134f49d
shashank201104 <shashanksaini201104@gmail.com>
2025-05-09 21:24:51 +0530

    final project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,11 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT;  
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,7 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log("bing bong")
     console.log("MongoDB connection error:", error);
   }
 };
